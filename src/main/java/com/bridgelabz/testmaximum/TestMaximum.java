4d7a1aa7fcfebf68d52b4ec9669777273f59e04f
nnalin04 <nnalin04@gmail.com>
2020-08-13 21:52:46 +0530

    Added: function to get the greatest float in the given floats.

--- src/main/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ src/main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -2,23 +2,24 @@ package com.bridgelabz.testmaximum;
 
 public class TestMaximum {
 
-    Integer num1;
-    Integer num2;
-    Integer num3;
-
-    TestMaximum(Integer num1, Integer num2, Integer num3){
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num3 = num3;
-    }
-
-    public Integer numberWithMaxValue(){
+    public Integer numberWithMaxValue(Integer num1, Integer num2, Integer num3){
         Integer max = num1;
         if(num2.compareTo(num1) > 0){
             max = num2;
         }
         if(num3.compareTo(num2) > 0){
             max = num3;
+        }
+        return max;
+    }
+
+    public Float floatWithMaxValue(Float float1, Float float2, Float float3){
+        Float max = float1;
+        if(float2.compareTo(float1) > 0){
+            max = float2;
+        }
+        if(float3.compareTo(float2) > 0){
+            max = float3;
         }
         return max;
     }
