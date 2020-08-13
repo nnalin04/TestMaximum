f90e9566964f794dc95fe728be8012e03c3d8ee6
nnalin04 <nnalin04@gmail.com>
2020-08-13 23:35:46 +0530

    Refactored: changed the 3 method to one generic method.

--- src/main/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ src/main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -2,30 +2,8 @@ package com.bridgelabz.testmaximum;
 
 public class TestMaximum {
 
-    public Integer numberWithMaxValue(Integer input1, Integer input2, Integer input3){
-        Integer max = input1;
-        if(input2.compareTo(max) > 0){
-            max = input2;
-        }
-        if(input3.compareTo(max) > 0){
-            max = input3;
-        }
-        return max;
-    }
-
-    public Float floatWithMaxValue(Float input1, Float input2, Float input3){
-        Float max = input1;
-        if(input2.compareTo(max) > 0){
-            max = input2;
-        }
-        if(input3.compareTo(max) > 0){
-            max = input3;
-        }
-        return max;
-    }
-
-    public String stringWithMaxValue(String input1, String input2, String input3){
-        String max = input1;
+    public <E extends Comparable> E inputWithMaxValue(E input1, E input2, E input3){
+        E max = input1;
         if(input2.compareTo(max) > 0){
             max = input2;
         }
--- src/test/java/com/bridgelabz/testmaximum/TestMaximumTests.java
+++ src/test/java/com/bridgelabz/testmaximum/TestMaximumTests.java
@@ -8,63 +8,63 @@ public class TestMaximumTests {
     @Test
     public void givenNumbers_1stPositionIsMax_shouldReturn1stNumber() {
         TestMaximum maximum = new TestMaximum();
-        int max = maximum.numberWithMaxValue(3, 2, 1);
+        int max = maximum.inputWithMaxValue(3, 2, 1);
         Assert.assertEquals(3, max);
     }
 
     @Test
     public void givenNumbers_2ndPositionIsMax_shouldReturn2ndNumber() {
         TestMaximum maximum = new TestMaximum();
-        int max = maximum.numberWithMaxValue(3, 2, 1);
+        int max = maximum.inputWithMaxValue(3, 2, 1);
         Assert.assertEquals(3, max);
     }
 
     @Test
     public void givenNumbers_3rdPositionIsMax_shouldReturn3rdNumber() {
         TestMaximum maximum = new TestMaximum();
-        int max = maximum.numberWithMaxValue(1, 2, 3);
+        int max = maximum.inputWithMaxValue(1, 2, 3);
         Assert.assertEquals(3, max);
     }
 
     @Test
     public void givenFloats_1stPositionIsMax_shouldReturn1stFloat() {
         TestMaximum maximum = new TestMaximum();
-        float max = maximum.floatWithMaxValue((float)3.3, (float)2.2, (float)2.2);
+        float max = maximum.inputWithMaxValue((float)3.3, (float)2.2, (float)2.2);
         Assert.assertEquals(3.3, max,1);
     }
 
     @Test
     public void givenFloats_2stPositionIsMax_shouldReturn2stFloat() {
         TestMaximum maximum = new TestMaximum();
-        double max = maximum.floatWithMaxValue((float)3.3, (float)2.2, (float)1.1);
+        double max = maximum.inputWithMaxValue((float)3.3, (float)2.2, (float)1.1);
         Assert.assertEquals(3.3, max, 1);
     }
 
     @Test
     public void givenFloats_3stPositionIsMax_shouldReturn3stFloat() {
         TestMaximum maximum = new TestMaximum();
-        double max = maximum.floatWithMaxValue((float)3.3, (float)2.3, (float)1.3);
+        double max = maximum.inputWithMaxValue((float)3.3, (float)2.3, (float)1.3);
         Assert.assertEquals(3.3, max, 1);
     }
 
     @Test
     public void givenString_1stPositionIsMax_shouldReturn1stString() {
         TestMaximum maximum = new TestMaximum();
-        String max = maximum.stringWithMaxValue("Peach", "Apple", "Banana");
+        String max = maximum.inputWithMaxValue("Peach", "Apple", "Banana");
         Assert.assertEquals("Peach", max);
     }
 
     @Test
     public void givenString_2stPositionIsMax_shouldReturn2stString() {
         TestMaximum maximum = new TestMaximum();
-        String max = maximum.stringWithMaxValue("Apple", "Peach", "Banana");
+        String max = maximum.inputWithMaxValue("Apple", "Peach", "Banana");
         Assert.assertEquals("Peach", max);
     }
 
     @Test
     public void givenString_3stPositionIsMax_shouldReturn3stString() {
         TestMaximum maximum = new TestMaximum();
-        String max = maximum.stringWithMaxValue("Apple", "Banana", "Peach");
+        String max = maximum.inputWithMaxValue("Apple", "Banana", "Peach");
         Assert.assertEquals("Peach", max);
     }
 }
